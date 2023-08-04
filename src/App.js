7a766f006fa539ae32561c748ec7330633dea2b3
selvakisar <130025654+selvakisar@users.noreply.github.com>
2023-08-04 15:58:39 +0530

    context

--- src/App.js
+++ src/App.js
@@ -3,49 +3,49 @@
 import { useState } from "react";
 import "./App.css";
 import Booklist from "./components/Booklist";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
 import Dashboard from "./page/Dashboard";
 import User from "./page/User";
 import AddBooks from "./components/AddBook";
+import Editbook from "./components/Editbook";
+import Nopage from "./page/Nopage";
 function App() {
-  const BookData = [
-    {
-      id: 1,
-      name: "Nation First",
-      Author: "V.P.Malik",
-      Language: "English",
-      Pages: "240",
-      Published: "2019",
-    },
-    {
-      id: 2,
-      name: " A Nation of Idiots",
-      Author: "Daksh tyagi",
-      Language: "English",
-      Pages: "300",
-      published: "2021",
-    },
-  ];
+  // const BookData = [
+  //   {
+  //     id: 1,
+  //     name: "Nation First",
+  //     Author: "V.P.Malik",
+  //     Language: "English",
+  //     Pages: "240",
+  //     Published: "2019",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: " A Nation of Idiots",
+  //     Author: "Daksh tyagi",
+  //     Language: "English",
+  //     Pages: "300", 
+  //     Published: "2021",
+  //   },
+  // ];
 
-  const [data, setData] = useState(BookData);
+  // const [data, setData] = useState(BookData);
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Dashboard />} />
 
-          <Route path="/users" element={<User/>}
-          />
-          <Route
-          />
-        <Route
-          path="/book/all"
-          element={<Booklist BookData={data} setData={setData} />}
-        />
+        <Route path="/users" element={<User />} />
+        <Route />
 
-<Route
-          path="/addbook"
-          element={<AddBooks/>}
-        />
+        {/* all books path  */}
+        <Route path="/book/all" element={<Booklist />} />
+
+        {/* Edit path */}
+        <Route path="/edit/:id" element={<Editbook />} />
+        <Route path="/addbook" element={<AddBooks />} />
+
+        <Route path="*" element={<Nopage />} />
       </Routes>
     </div>
   );
